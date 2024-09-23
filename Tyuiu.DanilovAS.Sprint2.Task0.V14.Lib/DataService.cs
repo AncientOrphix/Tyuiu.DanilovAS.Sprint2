a57c1ba3cd8098d2c7297b6f3e96f157d1d7d887
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2024-09-24 00:06:53 +0500

    Add project files.

---
+++ Tyuiu.DanilovAS.Sprint2.Task0.V14.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.DanilovAS.Sprint2.Task0.V14.Lib
+{
+    public class DataService : ISprint2Task0V14
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            DataService ds = new DataService();
+            bool[] res = new bool[6];
+
+            res[0] = x == 1075;
+            res[1] = y != 754;
+            res[2] = y < x;
+            res[3] = y > x;
+            res[4] = y <= x;
+            res[5] = y >= x;
+
+            return res;
+        }
+    }
+}
